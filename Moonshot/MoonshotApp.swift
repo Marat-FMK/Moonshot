aa7f7055e965f139e96c43c4a53a91c282be1651
MaratXcode <kosjachok8@yandex.ru>
2024-09-28 17:12:18 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Marat Fakhrizhanov on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
